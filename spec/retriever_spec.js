54c198bbeeb2e4036cba067c94831f4fd62ee890
Jamie Hale <jamie@smallarmyofnerds.com>
2016-12-18 11:25:25 -0500

    Complete the retriever spec for single file retrieval

--- spec/retriever_spec.js
+++ spec/retriever_spec.js
@@ -6,6 +6,59 @@ chai.use(sinonChai);
 
 var Retriever = require('../src/retriever');
 
-describe('Retriever', function() {
+var sandbox;
+var ipfsLink;
+var metadataGatherer;
+var retriever;
+var fakeMetadata;
+var filename = '/path/to/file';
 
+describe('Retriever', function() {
+    describe('getMetadataFor', function() {
+        beforeEach(function() {
+            sandbox = sinon.sandbox.create();
+            fakeMetadata = {
+                tag1: 'abc',
+                tag2: 'def'
+            };
+            ipfsLink = {
+                pinFileByHash: function(ipfsHash, callback) {
+                    callback();
+                },
+                getLocalPathByHash: function(ipfsHash, callback) {
+                    callback(filename);
+                }
+            };
+            metadataGatherer = {
+                gatherFor: function(path, callback) {
+                    callback(fakeMetadata);
+                }
+            };
+            retriever = new Retriever(ipfsLink, metadataGatherer);
+        });
+        afterEach(function() {
+            sandbox.restore();
+        });
+
+        it('pins the file in ipfs', function() {
+            ipfsLink.pinFileByHash = sinon.spy();
+            retriever.getMetadataFor('abcd', () => {});
+            expect(ipfsLink.pinFileByHash).to.have.been.calledWith('abcd');
+        });
+        it('finds the local path to the file', function() {
+            ipfsLink.getLocalPathByHash = sinon.spy();
+            retriever.getMetadataFor('abcd', () => {});
+            expect(ipfsLink.getLocalPathByHash).to.have.been.calledWith('abcd');
+        });
+        it('requests the metadata from the file', function() {
+            metadataGatherer.gatherFor = sinon.spy();
+            retriever.getMetadataFor('abcd', () => {});
+            expect(metadataGatherer.gatherFor).to.have.been.calledWith(filename);
+        });
+        it('returns the metadata for the file', function() {
+            retriever.getMetadataFor('abcd', function(metadata) {
+                expect(metadata).to.eql(fakeMetadata);
+            });
+        });
+    });
 });
--- src/retriever.js
+++ src/retriever.js
@@ -1,4 +1,19 @@
-function Retriever() {
+var ipfsLink;
+var metadataGatherer;
+
+function Retriever(_ipfsLink, _metadataGatherer) {
+    ipfsLink = _ipfsLink;
+    metadataGatherer = _metadataGatherer;
+}
+
+Retriever.prototype.getMetadataFor = function(ipfsHash, callback) {
+    ipfsLink.pinFileByHash(ipfsHash, function() {
+        ipfsLink.getLocalPathByHash(ipfsHash, function(path) {
+            metadataGatherer.gatherFor(path, function(metadata) {
+                callback(metadata);
+            });
+        });
+    });
 }
 
 module.exports = Retriever;
