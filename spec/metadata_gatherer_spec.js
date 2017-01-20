d1808e5f3eadee25dfe4a81e9e3aa9fa20043794
Jamie Hale <jamie@smallarmyofnerds.com>
2017-01-20 08:19:13 -0500

    Add MetadataGatherer.gatherFor to extract manifest

--- spec/metadata_gatherer_spec.js
+++ spec/metadata_gatherer_spec.js
@@ -9,4 +9,92 @@ var expect = chai.expect;
 var MetadataGatherer = require('../src/metadata_gatherer');
 
 describe('MetadataGatherer', function() {
+
+    describe('gatherFor', function() {
+
+        var mockManifest;
+        var mockPayload;
+        
+        beforeEach(function() {
+            mockManifest = {
+                FileName: 'Louie.jpeg',
+                EthereumBlockHash: '0xe7f9a8373326ecb3240b0c6743c4792da4a17207cf0da4056c3fad94766fc62f',
+                EthereumBlockNumber: 2777500,
+                GUID: '3a4334be-e41f-49c8-8518-340f74277606',
+                BitcoinBlockHash: '0000000000000000036d5a7173fa75185a5f7b60195d3bac03ecef3e971c1637',
+                BitcoinBlockNumber: 442677,
+                FileHashes: '84C5B7886D243D0ADBB3C707B629F3BF',
+                PreviousFileHashes: '84C5B7886D243D0ADBB3C707B629F3C0',
+                PreviousIPFSHash: 'Qmb7Uwc39Q7YpPsfkWj54S2rMgdV6D845Sgr75GyxZfV4W'
+            };
+            mockPayload = 12;
+
+            metadataGatherer = new MetadataGatherer();
+        });
+
+        it('stores the filename into the metadata', function(done) {
+            metadataGatherer.gatherFor(mockManifest, mockPayload).then(function(metadata) {
+                expect(metadata.filename).to.eq(mockManifest.FileName);
+                done();
+            }).catch(function(error) {
+                done(error);
+            });
+        });
+
+        it('stores the ethereum blockchain info into the metadata', function(done) {
+            metadataGatherer.gatherFor(mockManifest, mockPayload).then(function(metadata) {
+                expect(metadata.ethereumBlockHash).to.eq(mockManifest.EthereumBlockHash);
+                expect(metadata.ethereumBlockNumber).to.eq(mockManifest.EthereumBlockNumber);
+                done();
+            }).catch(function(error) {
+                done(error);
+            });
+        });
+
+        it('stores the guid into the metadata', function(done) {
+            metadataGatherer.gatherFor(mockManifest, mockPayload).then(function(metadata) {
+                expect(metadata.guid).to.eq(mockManifest.GUID);
+                done();
+            }).catch(function(error) {
+                done(error);
+            });
+        });
+
+        it('stores the Bitcoin blockchain info into the metadata', function(done) {
+            metadataGatherer.gatherFor(mockManifest, mockPayload).then(function(metadata) {
+                expect(metadata.bitcoinBlockHash).to.eq(mockManifest.BitcoinBlockHash);
+                expect(metadata.bitcoinBlockNumber).to.eq(mockManifest.BitcoinBlockNumber);
+                done();
+            }).catch(function(error) {
+                done(error);
+            });
+        });
+
+        it('stores the file hashes into the metadata', function(done) {
+            metadataGatherer.gatherFor(mockManifest, mockPayload).then(function(metadata) {
+                expect(metadata.fileHashes).to.eq(mockManifest.FileHashes);
+                done();
+            }).catch(function(error) {
+                done(error);
+            });
+        });
+
+        it('stores the previous file hashes into the metadata', function(done) {
+            metadataGatherer.gatherFor(mockManifest, mockPayload).then(function(metadata) {
+                expect(metadata.previousFileHashes).to.eq(mockManifest.PreviousFileHashes);
+                done();
+            }).catch(function(error) {
+                done(error);
+            });
+        });
+
+        it('stores the previous IPFS hash into the metadata', function(done) {
+            metadataGatherer.gatherFor(mockManifest, mockPayload).then(function(metadata) {
+                expect(metadata.previousIpfsHash).to.eq(mockManifest.PreviousIPFSHash);
+                done();
+            }).catch(function(error) {
+                done(error);
+            });
+        });
+    });
 });
--- src/metadata_gatherer.js
+++ src/metadata_gatherer.js
@@ -1,4 +1,18 @@
 var MetadataGatherer = function() {
 };
 
+MetadataGatherer.prototype.gatherFor = function(manifest, payload) {
+    return Promise.resolve({
+        filename: manifest.FileName,
+        ethereumBlockHash: manifest.EthereumBlockHash,
+        ethereumBlockNumber: manifest.EthereumBlockNumber,
+        guid: manifest.GUID,
+        bitcoinBlockHash: manifest.BitcoinBlockHash,
+        bitcoinBlockNumber: manifest.BitcoinBlockNumber,
+        fileHashes: manifest.FileHashes,
+        previousFileHashes: manifest.PreviousFileHashes,
+        previousIpfsHash: manifest.PreviousIPFSHash
+    });
+};
+
 module.exports = MetadataGatherer;
