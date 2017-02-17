b3fad36a672a2ff055dcf62f50875d9751c886b2
Jamie Hale <jamie@smallarmyofnerds.com>
2017-02-17 11:22:43 -0500

    Configuration-ethize dev_chain add_deposition

--- dev_chain/bin/add_deposition.js
+++ dev_chain/bin/add_deposition.js
@@ -5,16 +5,14 @@ const path = require('path');
 const multihash = require('multihashes');
 const bs58 = require('bs58');
 
+const configuration = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../config/config.json'), 'utf8')).development;
+
 var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+var web3 = new Web3(new Web3.providers.HttpProvider(configuration.ethereum.endpoint));
 
 var provenAbi = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../res/proven.abi'), 'utf8'));
-//var provenAddress = '0x3ff76874e26e00154c81854dab23d594e7fef480';
-var provenAddress = '0xc34bf56a27ceab53e795eba55b9f1503eea6a771';
 var Proven = web3.eth.contract(provenAbi);
-var proven = Proven.at(provenAddress);
-//var fromAddress = '0x0061b257BC2985c93868416f6543f76359AC1072';
-var fromAddress = '0x00F28F9B9692E00feAB5A53469FC3e2972574619';
+var proven = Proven.at(configuration.proven.address);
 
 function ipfsHashStringToHexString(ipfsHash) {
     let rawIpfsHash = bs58.decode(ipfsHash);
@@ -24,7 +22,7 @@ function ipfsHashStringToHexString(ipfsHash) {
 var ipfsHashAsHexString = '0x' + ipfsHashStringToHexString(process.argv[2]);
 console.log(ipfsHashAsHexString);
 
-proven.publishDeposition.sendTransaction(ipfsHashAsHexString, {from: fromAddress}, function(error, txHash) {
+proven.publishDeposition.sendTransaction(ipfsHashAsHexString, {from: configuration.ethereum.coinbase}, function(error, txHash) {
     console.log('Transaction sent...');
     if (error) {
         console.log(error);
